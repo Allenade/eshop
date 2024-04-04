c82610a56578391e6ba4b772c9d7265e0ec8e625
Allenade <allenumunade@gmail.com>
2024-04-04 14:31:37 +0100

    databaseChanges

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -4,16 +4,27 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyAOxPTh8p5Tpf7jDQbN74KvL95AeFL8bWE",
-  authDomain: "eshop-6e4a9.firebaseapp.com",
-  projectId: "eshop-6e4a9",
-  storageBucket: "eshop-6e4a9.appspot.com",
-  messagingSenderId: "24699082734",
-  appId: "1:24699082734:web:10fcd657c9469f89a420ff",
-};
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyAOxPTh8p5Tpf7jDQbN74KvL95AeFL8bWE",
+//   authDomain: "eshop-6e4a9.firebaseapp.com",
+//   projectId: "eshop-6e4a9",
+//   storageBucket: "eshop-6e4a9.appspot.com",
+//   messagingSenderId: "24699082734",
+//   appId: "1:24699082734:web:10fcd657c9469f89a420ff",
+// };
 
-// Initialize Firebase
+// // Initialize Firebase
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+export const firebaseConfig = {
+  apiKey: "AIzaSyDqkdNbWtm1vgM-ER_YRQqkmaJRrJraGnI",
+  authDomain: "allen-eshop.firebaseapp.com",
+  projectId: "allen-eshop",
+  storageBucket: "allen-eshop.appspot.com",
+  messagingSenderId: "788823055808",
+  appId: "1:788823055808:web:46a17a40ec79dd42083c25",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
